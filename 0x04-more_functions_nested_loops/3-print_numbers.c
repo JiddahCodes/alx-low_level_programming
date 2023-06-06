004646c46c570562eb191f29e82b8ce0a73180fa
JiddahCodes <ibrahimhauwa977@gmail.com>
2023-06-06 08:10:57 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_numbers - function that prints the numbers,
+ * print_numbers - A function that prints the numbers,
  * from 0 to 9, followed by a new line.
-**/
+ */
 
 void print_numbers(void)
 {
