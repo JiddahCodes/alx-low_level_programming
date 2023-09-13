5fd0ea22ff1826cf7195aeb809494d129ed7448f
JiddahCodes <ibrahimhauwa977@gmail.com>
2023-09-13 12:21:00 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,12 @@
 void print_alphabet_x10(void)
 {
 	char alpha;
-	int i = 0;
+	int i;
+	i = 0;
 
 	while (i < 10)
 	{
-		for (alpha = 'a'; alpha < 'z'; alpha++)
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
 			_putchar(alpha);
 		}
